8530c77087bf77494ae485fdd643b518e332c238
Jessica Antunes <jessantunes@protonmail.com>
2021-03-04 17:55:24 -0500

    commit tree f4b1f745faaab16b0e1fa67df8c280d5a6e8d090

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,56 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{ | song | song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(filename)
+    artist_song = filename[0..-5].split(" - ")
+    song = self.new
+    song.name = artist_song[1]
+    song.artist_name = artist_song[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    artist_song = filename[0..-5].split(" - ")
+    song = self.create
+    song.name = artist_song[1]
+    song.artist_name = artist_song[0]
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
